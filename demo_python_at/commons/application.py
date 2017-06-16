012ddb1b816ca806ee7a7d837062a3820a84c69a
Ivan Podorozhnyi <ivan.podorozhnyi@gmail.com>
2017-06-16 14:20:20 +0300

    Add class docstrings
    Changes affect classes: Application, SimpleApplication, Message, StrMessage, HtmlMessage, FakeMessage, Printer and StdoutPrinter.
    
    Affects #24

--- demo_python_at/commons/application.py
+++ demo_python_at/commons/application.py
@@ -5,12 +5,23 @@ from demo_python_at.commons.printer import Printer
 
 
 class Application(ABC):
+
+    """Base class for all applications."""
+
     @abstractmethod
     def print(self):
         pass
 
 
 class SimpleApplication(Application):
+
+    """
+    Class that simply prints out text.
+
+    It aggregates the Printer and Message class instances and
+    performs printing the content of Message instance.
+    """
+
     def __init__(self, printer: Printer, message: Message):
         self._printer = printer
         self._message = message
