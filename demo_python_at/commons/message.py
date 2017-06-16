d87add26fc9a8a1514f5cebd86a823b518f56c23
Anton Karabaza <akarab@macmini561.ddns.softservecom.com>
2017-06-16 17:08:58 +0300

    Add docstrings for public methods
    Changes affect modules in demo_python_at.commons package.
    
    Affects #25

--- demo_python_at/commons/message.py
+++ demo_python_at/commons/message.py
@@ -7,10 +7,12 @@ class Message(ABC):
 
     @abstractmethod
     def data(self) -> str:
+        """Abstract method to return message data in string format."""
         pass
 
     @abstractmethod
     def exist(self) -> bool:
+        """Abstract method to verify if message exist and return bool."""
         pass
 
 
@@ -19,12 +21,28 @@ class StrMessage(Message):
     """Class that stores a message in string format."""
 
     def __init__(self, message: str):
+        """
+        Initialize StrMessage object.
+
+        :param message: data in string format
+        """
         self._message = message
 
     def exist(self) -> bool:
+        """
+        Verify that message length is greater than 0.
+
+        :return: True if message length is greater than 0
+                 False in other cases
+        """
         return len(self._message) > 0
 
     def data(self) -> str:
+        """
+        Get message data.
+
+        :return: message data in string format
+        """
         return self._message
 
 
@@ -33,12 +51,27 @@ class HtmlMessage(Message):
     """Class that stores a message in html format."""
 
     def __init__(self, base: Message):
+        """
+        Initialize HtmlMessage object.
+
+        :param base: Message class object
+        """
         self._message = base
 
     def data(self):
+        """
+        Reformat message data in HTML paragraph.
+
+        :return: reformatted message data in string format
+        """
         return "<p>{m}</p>".format(m=self._message.data())
 
     def exist(self):
+        """
+        Verify if message exists.
+
+        :return: existence state of message as bool
+        """
         return self._message.exist()
 
 
@@ -51,11 +84,27 @@ class FakeMessage(Message):
     """
 
     def __init__(self, message: str = "", exist: bool = False):
+        """
+        Initialize FakeMessage object.
+
+        :param message: data in string format or "" by default
+        :param exist: whether data exist or False by default
+        """
         self._message = message
         self._exist = exist
 
     def exist(self) -> bool:
+        """
+        Get existence state of message.
+
+        :return: existence state of message as bool
+        """
         return self._exist
 
     def data(self) -> str:
+        """
+        Get message data.
+
+        :return: message data in string format
+        """
         return self._message
